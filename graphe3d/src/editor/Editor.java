2a1700920144f429c66c377e475c045568da7cf1
kutoriku <kutoriku>
2007-02-18 18:38:39 +0000

    Initial revision

---
+++ graphe3d/src/editor/Editor.java
@@ -0,0 +1,18 @@
+package editor;
+
+/**
+ * Classe of the editor.
+ * 
+ * @author Jerome Catric
+ */
+public class Editor {
+	
+	/**
+	 * Main procedure.
+	 * 
+	 * @param args list of arguments.
+	 */
+	public static void main(String[] args){
+		System.out.println("Editor of API Graphe 3D");
+	}
+}
