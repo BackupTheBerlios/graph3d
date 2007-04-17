f2e614685bfd2d5a7d1ed5d111f8f89cc3e7e5d1
kutoriku <kutoriku>
2007-04-17 18:02:23 +0000

    *** empty log message ***

--- graphe3d/src/graph3dTest/elementsTest/GNodeTest.java
+++ graphe3d/src/graph3dTest/elementsTest/GNodeTest.java
@@ -3,6 +3,8 @@
  */
 package graph3dTest.elementsTest;
 
+import java.util.Hashtable;
+
 import graph3d.elements.GNode;
 import junit.framework.TestCase;
 
@@ -74,7 +76,17 @@ public class GNodeTest extends TestCase {
 	 * Test method for {@link graph3d.elements.GNode#setAttributes(java.util.Hashtable)}.
 	 */
 	public void testSetAttributes() {
-		fail("Not yet implemented");
+		// Création des éléments à tester :
+		GNode node = new GNode("node");
+		
+		// Modification des attribues
+		String[] data = new String[]{"ip","int","192.168.1.1"};
+		Hashtable<String, String[]> attributes = new Hashtable<String, String[]>();
+		attributes.put("ip", data);
+		node.setAttributes(attributes);	
+		
+		//Verification que l'attribut est bien présent.
+		assertEquals(node.getAttributeByName("ip"),data);	
 	}
 
 	/**
@@ -117,28 +129,48 @@ public class GNodeTest extends TestCase {
 	 * Test method for {@link graph3d.elements.GNode#setCoordonnateX(float)}.
 	 */
 	public void testSetCoordonnateX() {
-		fail("Not yet implemented");
+		// Création des éléments à tester :
+		GNode node = new GNode("node");
+		// Modification de la coordonnée X
+		node.setCoordonnateX(3.5f);
+		// Vérification de la modification
+		assertEquals(node.getCoordonnateX(),3.5f);
 	}
 
 	/**
 	 * Test method for {@link graph3d.elements.GNode#setCoordonnateY(float)}.
 	 */
 	public void testSetCoordonnateY() {
-		fail("Not yet implemented");
+		// Création des éléments à tester :
+		GNode node = new GNode("node");
+		// Modification de la coordonnée X
+		node.setCoordonnateY(3.5f);
+		// Vérification de la modification
+		assertEquals(node.getCoordonnateY(),3.5f);
 	}
 
 	/**
 	 * Test method for {@link graph3d.elements.GNode#setCoordonnateZ(float)}.
 	 */
 	public void testSetCoordonnateZ() {
-		fail("Not yet implemented");
+		// Création des éléments à tester :
+		GNode node = new GNode("node");
+		// Modification de la coordonnée X
+		node.setCoordonnateZ(3.5f);
+		// Vérification de la modification
+		assertEquals(node.getCoordonnateZ(),3.5f);
 	}
 
 	/**
 	 * Test method for {@link graph3d.elements.GNode#setRadius(float)}.
 	 */
 	public void testSetRadius() {
-		fail("Not yet implemented");
+		// Création des éléments à tester :
+		GNode node = new GNode("node");
+		// Modification de la coordonnée X
+		node.setRadius(3.5f);
+		// Vérification de la modification
+		assertEquals(node.getRadius(),3.5f);
 	}
 	
 
@@ -160,7 +192,10 @@ public class GNodeTest extends TestCase {
 	 * Test method for {@link graph3d.elements.GNode#getName()}.
 	 */
 	public void testGetName() {
-		fail("Not yet implemented");
+		// Création des éléments à tester :
+		GNode node = new GNode("node");
+		// Verification du nom du noeud
+		assertEquals(node.getName(), "node");
 	}
 
 	/**
@@ -181,35 +216,50 @@ public class GNodeTest extends TestCase {
 	 * Test method for {@link graph3d.elements.GNode#getCoordonnateX()}.
 	 */
 	public void testGetCoordonnateX() {
-		fail("Not yet implemented");
+		// Création des éléments à tester :
+		GNode node = new GNode("node",2.5f,3.4f,6.4f);
+		// Verification de la valeur de la coordonnee X
+		assertEquals(node.getCoordonnateX(), 2.5f);
 	}
 
 	/**
 	 * Test method for {@link graph3d.elements.GNode#getCoordonnateY()}.
 	 */
 	public void testGetCoordonnateY() {
-		fail("Not yet implemented");
+		// Création des éléments à tester :
+		GNode node = new GNode("node",2.5f,3.4f,6.4f);
+		// Verification de la valeur de la coordonnee Y
+		assertEquals(node.getCoordonnateY(), 3.4f);;
 	}
 
 	/**
 	 * Test method for {@link graph3d.elements.GNode#getCoordonnateZ()}.
 	 */
 	public void testGetCoordonnateZ() {
-		fail("Not yet implemented");
+		// Création des éléments à tester :
+		GNode node = new GNode("node",2.5f,3.4f,6.4f);
+		// Verification de la valeur de la coordonnee Z
+		assertEquals(node.getCoordonnateZ(), 6.4f);
 	}
 
 	/**
 	 * Test method for {@link graph3d.elements.GNode#getRadius()}.
 	 */
 	public void testGetRadius() {
-		fail("Not yet implemented");
+		// Création des éléments à tester :
+		GNode node = new GNode("node",2.5f,3.4f,6.4f,5f);
+		// Verification de la valeur du rayon d'une sphere
+		assertEquals(node.getRadius(), 5f);
 	}
 
 	/**
 	 * Test method for {@link graph3d.elements.GNode#getDISTANCE()}.
 	 */
 	public void testGetDISTANCE() {
-		fail("Not yet implemented");
+		// Création des éléments à tester :
+		GNode node = new GNode("node");
+		// Verification de la distance entre 2 spheres
+		assertEquals(node.getDISTANCE(), 2f);
 	}
 
 
