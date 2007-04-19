fe818bc9ac17ce3f3b4f67374e2f3c711079418c
e0400640 <e0400640>
2007-04-19 07:36:13 +0000

    optimisation de la fonction pour la construction de la meilleure vue

--- graphe3d/src/graph3d/universe/GGrapheUniverse.java
+++ graphe3d/src/graph3d/universe/GGrapheUniverse.java
@@ -83,22 +83,22 @@ public class GGrapheUniverse extends VirtualUniverse{
 		float fieldOfView = this.view.getFieldOfView();
 	
 		
-		float min=0, max = 0, minZ = 0, maxZ = 0;
+		float minX=0, maxX = 0, minY = 0, maxY = 0, minZ = 0, maxZ = 0;
 		
 		Enumeration<String> keys = this.graph.getNodes().keys();
 		while (keys.hasMoreElements()) {
 			String key = keys.nextElement();
 			GNode node = this.graph.getNode(key);
 			
-			if (min > node.getCoordonnateX()) {
-				min = node.getCoordonnateX();
-			} else if (max < node.getCoordonnateX()) {
-				max = node.getCoordonnateX();
+			if (minX > node.getCoordonnateX()) {
+				minX = node.getCoordonnateX();
+			} else if (maxX < node.getCoordonnateX()) {
+				maxX = node.getCoordonnateX();
 			}
-			if (min > node.getCoordonnateY()) {
-				min = node.getCoordonnateY();
-			} else if (max < node.getCoordonnateY()) {
-				max = node.getCoordonnateY();
+			if (minY > node.getCoordonnateY()) {
+				minY = node.getCoordonnateY();
+			} else if (maxY < node.getCoordonnateY()) {
+				maxY = node.getCoordonnateY();
 			}
 			if (minZ > node.getCoordonnateZ()) {
 				minZ = node.getCoordonnateZ();
@@ -109,14 +109,14 @@ public class GGrapheUniverse extends VirtualUniverse{
 		
 		//construction des points extrèmes qui sont le plus proche de la caméra
 		//si ces points passent dans la vue les autres points existant aussi.
-		float[] xyZ = new float [] {min, min, maxZ};
-		float[] xYZ = new float [] {min, max, maxZ};
-		//float[] XYZ = new float [] {maxX, maxX, maxZ};
-		//float[] XyZ = new float [] {maxX, minX, maxZ};
-		//les deux derniers points ne sont pas utiles d'où le commentaire
+		float[] xyZ = new float [] {minX, minY, maxZ};
+		float[] xYZ = new float [] {minX, maxY, maxZ};
+		//float[] XYZ = new float [] {maxX, maxY, maxZ}; ce point n'est pas utilsé d'où le commentaire
+		float[] XyZ = new float [] {maxX, minY, maxZ};
+		
 		
 		//calcul du barycentre de ces points pour connaitre X et Y que l'on recherche pour la caméra
-		float[] barycenter = new float[] {(min + max) / 2, (min + max) / 2, maxZ};
+		float[] barycenter = new float[] {(minX + maxX) / 2, (minY + maxY) / 2, maxZ};
 		
 		//calcul de la distance nécessaire pour voir les 4 points
 		//cette distance correspond à la distance entre le barycentre précédemment calculé et le point où doit se situer la caméra.
@@ -137,16 +137,36 @@ public class GGrapheUniverse extends VirtualUniverse{
 		
 		//la base correspond à la longueur entre le barycentre et l'extrémité du champ de vision.
 		// cette extrémité se trouve sur l'un des 4 côté que forme les 4 points précédemment calculés.
-		float[] base = new float[3] ;
-		//calcul de X de la base
-		base[0] = xyZ[0] + ((xYZ[0] - xyZ[0]) / 2);
-		//calcul de Y de la base
-		base[1] = xyZ[1] + ((xYZ[1] - xyZ[1]) / 2);
-		//calcul de Z de la base
-		base[2] = xyZ[2] + ((xYZ[2] - xyZ[2]) / 2);
 		
-		//calcul de la distance
-		float lengthBetween = this.getLengthBetween(barycenter, base);
+		//sachant que les 4 points qui ont été calculés représente le sommet d'un rectangle
+		//la base du triangle n'est pas obligatoirement égale entre tous les côtés.
+		//C'est pourquoi il faut calculer deux bases et prendre la plus longue.
+		
+		//première base
+		float[] base1 = new float[3] ;
+		//calcul de X de la base
+		base1[0] = xyZ[0] + ((xYZ[0] - xyZ[0]) / 2);
+		//calcul de Y de la base
+		base1[1] = xyZ[1] + ((xYZ[1] - xyZ[1]) / 2);
+		//calcul de Z de la base
+		base1[2] = xyZ[2] + ((xYZ[2] - xyZ[2]) / 2);
+		
+		//second base
+		float[] base2 = new float[3] ;
+		//calcul de X de la base
+		base2[0] = xyZ[0] + ((XyZ[0] - xyZ[0]) / 2);
+		//calcul de Y de la base
+		base2[1] = xyZ[1] + ((XyZ[1] - xyZ[1]) / 2);
+		//calcul de Z de la base
+		base2[2] = xyZ[2] + ((XyZ[2] - xyZ[2]) / 2);
+		
+		//calcul de la longueur de la plus ongue base
+		float lengthBetween = 0;
+		if (this.getLengthBetween(barycenter, base1) > this.getLengthBetween(barycenter, base2)) {
+			lengthBetween = this.getLengthBetween(barycenter, base1);
+		} else {
+			lengthBetween = this.getLengthBetween(barycenter, base2);
+		}
 		
 		float length = (float)(lengthBetween / Math.tan(fieldOfView/ 2));
 		
