1703d081a227c88846b39ee928424afb1c6f92bd
e0400640 <e0400640>
2007-05-03 18:24:26 +0000

    mise à jour des panels d'intéractions et de cractéristiques

--- graphe3d/src/graph3d/use/PanelButtonInteraction.java
+++ graphe3d/src/graph3d/use/PanelButtonInteraction.java
@@ -3,110 +3,104 @@ package graph3d.use;
 import graph3d.universe.GGrapheUniverse;
 
 import java.awt.GridLayout;
+import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
-public class PanelButtonInteraction extends JPanel{
-	
-	private JButton bZoomPlus,bZoomMoins,boutonHaut,boutonGauche,boutonDroite,boutonBas,boutonCentrer;
-	private GGrapheUniverse universe;
-	
-	public PanelButtonInteraction(GGrapheUniverse universe){
-		
-		this.universe=universe;
-		
-	RecepBouton myRecepBouton=new RecepBouton();	
-	
-	// panel interaction 
-	JPanel panelDroiteBas=new JPanel();
-	panelDroiteBas.setLayout(new GridLayout(3,1,5,5));
-	TitledBorder borderDroiteBas = new TitledBorder(new EtchedBorder(),"Interaction");
-	panelDroiteBas.setBorder(borderDroiteBas);
+public class PanelButtonInteraction extends JPanel implements ActionListener {
 
-	
+	private JButton bZoomPlus, bZoomMoins, boutonHaut, boutonGauche,
+			boutonDroite, boutonBas, boutonCentrer;
+
+	private GGrapheUniverse universe;
+
+	public PanelButtonInteraction(GGrapheUniverse universe) {
+
+		this.universe = universe;
+
+		// panel interaction
+		this.setLayout(new GridLayout(3, 1));
+		TitledBorder borderDroiteBas = new TitledBorder(new EtchedBorder(),
+				"Interaction");
+		this.setBorder(borderDroiteBas);
+
 		// boutons du zoom
-		JPanel panelZoom=new JPanel();
-		panelZoom.setLayout(new GridLayout(1,2,5,5));
-		bZoomPlus =new JButton("+");
-		bZoomPlus.addActionListener(myRecepBouton);
+		JPanel panelZoom = new JPanel();
+		panelZoom.setLayout(new GridLayout(1, 2));
+		bZoomPlus = new JButton("+");
+		bZoomPlus.addActionListener(this);
 		panelZoom.add(bZoomPlus);
-			bZoomMoins =new JButton("-");
-			bZoomMoins.addActionListener(myRecepBouton);
-			panelZoom.add(bZoomMoins);
-		panelDroiteBas.add(panelZoom);
-			
+		bZoomMoins = new JButton("-");
+		bZoomMoins.addActionListener(this);
+		panelZoom.add(bZoomMoins);
+		this.add(panelZoom);
+
 		// boutons deplacement de la camera
-		JPanel panelInteraction=new JPanel();
-		panelInteraction.setLayout(new GridLayout(3,1));
+		JPanel panelInteraction = new JPanel();
+		panelInteraction.setLayout(new GridLayout(3, 1));
+
+		JPanel panInteracHaut = new JPanel();
+		panInteracHaut.setLayout(new GridLayout(1, 3));
 		
-			JPanel panInteracHaut=new JPanel();
-			panInteracHaut.setLayout(new GridLayout(1,3));
-				panInteracHaut.add(new JLabel(""));
-					boutonHaut =new JButton("^");
-					boutonHaut.addActionListener(myRecepBouton);
-					panInteracHaut.add(boutonHaut);
-						panInteracHaut.add(new JLabel(""));
-				panelInteraction.add(panInteracHaut);
-			JPanel panInteracMilieu=new JPanel();
-			panInteracMilieu.setLayout(new GridLayout(1,3));
-				boutonGauche =new JButton("<");
-				boutonGauche.addActionListener(myRecepBouton);
-				panInteracMilieu.add(boutonGauche);
-					panInteracMilieu.add(new JLabel(""));
-						boutonDroite =new JButton(">");
-						boutonDroite.addActionListener(myRecepBouton);
-						panInteracMilieu.add(boutonDroite);
-				panelInteraction.add(panInteracMilieu);
-			JPanel panInteracBas=new JPanel();
-			panInteracBas.setLayout(new GridLayout(1,3));
-				panInteracBas.add(new JLabel(""));
-					boutonBas=new JButton("v");
-					boutonBas.addActionListener(myRecepBouton);
-				 	panInteracBas.add(boutonBas);
-				 		panInteracBas.add(new JLabel(""));
-			panelInteraction.add(panInteracBas);
-			
-		panelDroiteBas.add(panelInteraction);
-		
+		panInteracHaut.add(new Label(""));
+		boutonHaut = new JButton("^");
+		boutonHaut.addActionListener(this);
+		panInteracHaut.add(boutonHaut);
+		panInteracHaut.add(new Label(""));
+		panelInteraction.add(panInteracHaut);
+		JPanel panInteracMilieu = new JPanel();
+		panInteracMilieu.setLayout(new GridLayout(1, 3));
+		boutonGauche = new JButton("<");
+		boutonGauche.addActionListener(this);
+		panInteracMilieu.add(boutonGauche);
+		panInteracMilieu.add(new Label(""));
+		boutonDroite = new JButton(">");
+		boutonDroite.addActionListener(this);
+		panInteracMilieu.add(boutonDroite);
+		panelInteraction.add(panInteracMilieu);
+		JPanel panInteracBas = new JPanel();
+		panInteracBas.setLayout(new GridLayout(1, 3));
+		panInteracBas.add(new Label(""));
+		boutonBas = new JButton("v");
+		boutonBas.addActionListener(this);
+		panInteracBas.add(boutonBas);
+		panInteracBas.add(new Label(""));
+		panelInteraction.add(panInteracBas);
+
+		this.add(panelInteraction);
+
 		// bouton centrer la vue
-		boutonCentrer=new JButton("Centrer la vue");
-		boutonCentrer.addActionListener(myRecepBouton);
-		panelDroiteBas.add(boutonCentrer);
-		
-	this.add(panelDroiteBas);
-	
+		boutonCentrer = new JButton("Centrer la vue");
+		boutonCentrer.addActionListener(this);
+		this.add(boutonCentrer);
 	}
-	
-	
-	class RecepBouton implements ActionListener{
-		public void actionPerformed(ActionEvent e){
-			if(e.getSource()==bZoomPlus){
-				universe.zoomMore();
-			}
-			if(e.getSource()==bZoomMoins){
-				universe.zoomLess();
-			}
-			if(e.getSource()==boutonHaut){
-				universe.rotateTop();
-			}
-			if(e.getSource()==boutonBas){
-				universe.rotateBottom();
-			}
-			if(e.getSource()==boutonGauche){
-				universe.rotateLeft();
-			}
-			if(e.getSource()==boutonDroite){
-				universe.rotateRight();
-			}
-			if(e.getSource()==boutonCentrer){
-				universe.centerView();
-			}
+
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == bZoomPlus) {
+			universe.zoomMore();
+		}
+		if (e.getSource() == bZoomMoins) {
+			universe.zoomLess();
+		}
+		if (e.getSource() == boutonHaut) {
+			universe.rotateTop();
+		}
+		if (e.getSource() == boutonBas) {
+			universe.rotateBottom();
+		}
+		if (e.getSource() == boutonGauche) {
+			universe.rotateLeft();
+		}
+		if (e.getSource() == boutonDroite) {
+			universe.rotateRight();
+		}
+		if (e.getSource() == boutonCentrer) {
+			universe.centerView();
 		}
 	}
 }
--- graphe3d/src/graph3d/use/PanelCaracteristique.java
+++ graphe3d/src/graph3d/use/PanelCaracteristique.java
@@ -1,26 +1,37 @@
 package graph3d.use;
 
-import java.awt.Dimension;
+import graph3d.exception.ASCIIFileNotFoundException;
+import graph3d.exception.GException;
+import graph3d.lists.GAttributesList;
+import graph3d.lists.GConnectionsList;
+import graph3d.universe.GGrapheUniverse;
+
 import java.awt.GridLayout;
 
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EtchedBorder;
 import javax.swing.border.TitledBorder;
 
 public class PanelCaracteristique extends JPanel{
 	
-	public PanelCaracteristique(){
-	
-		// panel caractéristiques
-		JPanel panelDroiteHaut=new JPanel();
-		panelDroiteHaut.setLayout(new GridLayout(2,1));
+	public PanelCaracteristique(GGrapheUniverse universe){
+		this.setLayout(new GridLayout(2,1));
 		TitledBorder borderDroiteHaut = new TitledBorder(new EtchedBorder(),"Caractéristiques");
-		panelDroiteHaut.setBorder(borderDroiteHaut);
+		this.setBorder(borderDroiteHaut);
 			
-			panelDroiteHaut.add(new JLabel("A completer ..."));
-			
-		this.add(panelDroiteHaut);
+		GAttributesList attributesList;
+		GConnectionsList connectionsList;
+		try {
+			attributesList = new GAttributesList(universe, "");
+			universe.addSelectionBehavior(attributesList);
+			this.add(attributesList);
+			connectionsList = new GConnectionsList();
+			attributesList.attachConnectionsList(connectionsList);
+			this.add(connectionsList);
+		} catch (GException e) {
+			e.printStackTrace();
+			e.showError();
+		}
 	
 	}
 
